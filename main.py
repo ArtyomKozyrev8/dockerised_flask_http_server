5aef47b48ef20735e00944def5cb15f290154912
Artyom Kozyrev <43035134+ArtyomKozyrev1989@users.noreply.github.com>
2020-07-07 18:09:44 +0300

    some logging

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from libs import mylib
 from flask import Flask
+import logging
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
+    logging.info("/")
     return "Hello World from Flask"
 
 
 @app.route("/x")
 def x():
+    logging.info("/x")
     return mylib.do_some_work()
 
 
 @app.route("/y")
 def y():
+    logging.info("/y")
     return mylib.do_another()
 
 
