90991b8e5a96fee46dd721397fdc94109ccfe93d
Artyom Kozyrev <43035134+ArtyomKozyrev1989@users.noreply.github.com>
2020-07-08 20:57:14 +0300

    added custom gunicorn setup file 5

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -29,7 +29,7 @@ loglevel = use_loglevel
 workers = 20
 bind = use_bind
 keepalive = 120
-errorlog = "/usr/src/app/logs"
+errorlog = "/usr/src/app/logs/log.txt"
 
 # For debugging and testing
 log_data = {
